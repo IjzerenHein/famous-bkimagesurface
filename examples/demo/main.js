d01b9061cee5907e6472eb995a91c3a54d3b6261
IjzerenHein <hrutjes@gmail.com>
2014-07-10 14:54:05 +0200

    Updated demo to also show PositionMode and RepeatMode.

--- examples/demo/main.js
+++ examples/demo/main.js
@@ -43,24 +43,46 @@ define(function(require) {
     // Create grid-layout
     var renderables = [];
     var grid = new GridLayout({
-        dimensions: [4, 3]
+        dimensions: [3, 4]
     });
     grid.sequenceFrom(renderables);
 
     // Demo sizing-modes
     var url = 'images/earth.png';
-    var sizeMode;
-    for (sizeMode in BkImageSurface.SizeMode) {
+    function _addImage(sizeMode, positionMode, repeatMode) {
         var bkImage = new BkImageSurface({
             content: url,
-            sizeMode: BkImageSurface.SizeMode[sizeMode],
+            sizeMode: sizeMode,
+            positionMode: positionMode,
+            repeatMode: repeatMode,
             properties: {
                 backgroundColor: 'black'
             }
         });
 
+        var desc = '';
+        if (sizeMode) {
+            for (var prop in BkImageSurface.SizeMode) {
+                if (BkImageSurface.SizeMode[prop] === sizeMode) sizeMode = prop;
+            }
+            desc += 'SizeMode: ' + sizeMode;
+        }
+        if (positionMode) {
+            for (var prop in BkImageSurface.PositionMode) {
+                if (BkImageSurface.PositionMode[prop] === positionMode) positionMode = prop;
+            }
+            if (desc.length) desc += '<br>';
+            desc += 'PositionMode: ' + positionMode;
+        }
+        if (repeatMode) {
+            for (var prop in BkImageSurface.RepeatMode) {
+                if (BkImageSurface.RepeatMode[prop] === repeatMode) repeatMode = prop;
+            }
+            if (desc.length) desc += '<br>';
+            desc += 'RepeatMode: ' + repeatMode;
+        }
         var text = new Surface({
-            content: 'SizeMode.' + sizeMode,
+            content: desc,
             properties: {
                 color: 'white',
                 padding: '10px',
@@ -74,6 +96,22 @@ define(function(require) {
         renderNode.add(text);
         renderables.push(renderNode);
     }
+
+    _addImage(BkImageSurface.SizeMode.ASPECTFIT);
+    _addImage(BkImageSurface.SizeMode.ASPECTFIT, BkImageSurface.PositionMode.LEFT);
+    _addImage(BkImageSurface.SizeMode.ASPECTFIT, null, BkImageSurface.RepeatMode.BOTH);
+
+    _addImage(BkImageSurface.SizeMode.ASPECTFILL);
+    _addImage(BkImageSurface.SizeMode.ASPECTFILL, BkImageSurface.PositionMode.TOP);
+    _addImage(BkImageSurface.SizeMode.FILL);
+
+    _addImage(BkImageSurface.SizeMode.AUTO);
+    _addImage(BkImageSurface.SizeMode.AUTO, BkImageSurface.PositionMode.BOTTOMRIGHT);
+    _addImage(BkImageSurface.SizeMode.AUTO, null, BkImageSurface.RepeatMode.VERTICAL);
+
+    _addImage('50px 50px');
+    _addImage('50px 50px', BkImageSurface.PositionMode.TOPLEFT);
+    _addImage('50px 50px', null, BkImageSurface.RepeatMode.BOTH);
 
     mainContext.add(grid);
 });
